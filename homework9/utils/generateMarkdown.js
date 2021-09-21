e80a2695b9364c34a776cd8889d3a55f79c82a99
soobin lim <sblim.monterrey@gmail.com>
2021-09-21 17:07:35 -0500

    add badge url from img.shields.io

--- homework9/utils/generateMarkdown.js
+++ homework9/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return 'https://img.shields.io/github/issues/soobin-lim/homework9.git';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +15,8 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+and next 
 `;
 }
-const pie = 'apple';
-module.exports = {generateMarkdown, pie};
+
+module.exports = {generateMarkdown};
